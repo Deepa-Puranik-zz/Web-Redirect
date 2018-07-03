7d08c9486133457ad5475bc1b595452ed4dfa12d
Deepa Puranik <Deepa.Puranik@advancedindia.onmicrosoft.com>
2018-07-03 17:58:33 +0530

    added web-redirect page and styles

---
+++ mosaic-styles/js/open-sans.js
@@ -0,0 +1,6 @@
+var gf = document.createElement('link')
+gf.rel = 'stylesheet'
+gf.href = 'https://fonts.googleapis.com/css?family=Open+Sans:400,700'
+gf.type = 'text/css'
+var gd = document.getElementsByTagName('link')[0]
+gd.parentNode.insertBefore(gf, gd)
